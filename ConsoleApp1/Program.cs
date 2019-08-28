d25f04daa6e2d437e863557c24daca6e7e5a9635
atilla <attilakaradeniz@gmail.com>
2019-08-28 18:59:30 +0200

    Added to ican repository

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BigInteger numberOne = 1;
+            BigInteger numberTwo = 1;
+            BigInteger result;
+            int counter = 1;
+
+            Console.WriteLine(numberOne + " (" + counter + ".sayı)");
+            for (int i = 0; i < 999; i++)
+            {
+                counter++;
+                result = numberOne + numberTwo;
+                Console.WriteLine(result + " (" + counter + ".sayı)");
+                numberOne = numberTwo;
+                numberTwo = result;
+            }
+
+            
+
+
+        }
+    }
+}
